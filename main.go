9cece7ca3770d2e676c5d56b73da13ec5abac6ae
AshwinAnoop <ashwinka999@gmail.com>
2022-03-08 14:24:56 +0530

    GET comined data of yaml and json

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -15,6 +16,8 @@ type ParamInfo struct {
 	Is_default      string `yaml:"IS_DEFAULT"`
 	Parameter_type  string `yaml:"PARAMETER_TYPE"`
 	Parameter_value string `yaml:"PARAMETER_VALUE"`
+	Level           string
+	Rank            int64
 }
 type ServerInfo struct {
 	Server              string      `yaml:"SERVER"`
@@ -38,6 +41,7 @@ func mainPage(c *gin.Context) {
 	})
 }
 
+// Get handler to return comined data of yaml and json file
 func getServerDetails(c *gin.Context) {
 
 	yfile, err := ioutil.ReadFile("test.yaml")
@@ -51,10 +55,43 @@ func getServerDetails(c *gin.Context) {
 		log.Fatal(err2)
 	}
 
+	jfile, err3 := ioutil.ReadFile("test.json")
+	if err3 != nil {
+		log.Fatal(err3)
+	}
+
+	var jdata map[string]interface{}
+	err4 := json.Unmarshal(jfile, &jdata)
+	if err4 != nil {
+		log.Fatal(err4)
+	}
+
+	fmt.Print("jdata : ", jdata)
+	var paramName string
+	for i := range data {
+		for j := range data[i].Server_params {
+			paramName = data[i].Server_params[j].Parameter_name
+			if val, ok := jdata[paramName]; ok {
+
+				m, _ := val.(map[string]interface{})
+
+				fmt.Print(m)
+				for k, v := range m {
+					if k == "Level" {
+						data[i].Server_params[j].Level = fmt.Sprint(v)
+
+					}
+					if k == "Rank" {
+						data[i].Server_params[j].Rank = int64(v.(float64))
+					}
+				}
+			}
+		}
+	}
+
 	server := c.Query("server")
 	for _, a := range data {
 		if a.Server == server {
-			fmt.Print(a)
 			c.HTML(http.StatusOK, "serverInfo.tmpl", gin.H{
 				"a": a,
 			})
